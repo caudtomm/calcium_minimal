c4c80ed0c743d10af93c41e5592cff9f7576a5f8
caudtomm <59736545+caudtomm@users.noreply.github.com>
2025-07-11 13:30:56 +0200

    small changes to plotting

--- plotting/plotUtils/similarityDynamics.m
+++ plotting/plotUtils/similarityDynamics.m
@@ -18,13 +18,14 @@ end
 
 % extract relevant data
 nplots = numel(out{1});
-data = nan(nwindows,nplots,2); % dim3: [mean, sem]
+data = nan(nwindows,nplots,3); % dim3: [mean, sem, std]
 for i_w = 1:nwindows
     for i_p = 1:nplots
         thisplot = out{i_w}{i_p};
         if isempty(thisplot); continue; end
         data(i_w,i_p,1) = mean(thisplot.data,'omitmissing'); % mean
-        data(i_w,i_p,2) = std(thisplot.data,[],'omitmissing')/numel(thisplot.data);
+        data(i_w,i_p,2) = std(thisplot.data,[],'omitmissing')/numel(thisplot.data); % sem
+        data(i_w,i_p,3) = std(thisplot.data,[],'omitmissing'); % std
     end
 end
 
@@ -74,35 +75,50 @@ tstart = tstart + 1; % <- actually, the immediately following frame
 
 idx = find(idx_by_stimgroup==6 & idx_by_subjectgroup==2); % all familiar & trained
 y1 = data(:,idx,1);
+err_y1 = data(:,idx,3);
 [~,tend1] = max(y1);
-[params1, ~, ~] = fitExpSaturation(t(tstart:tend1), y1(tstart:tend1), 0);
+% tend1 = tend1+1;
+[params1, yfit1, ~] = fitExpSaturation(t(tstart:tend1), y1(tstart:tend1), 0);
 
 idx = find(idx_by_stimgroup==7 & idx_by_subjectgroup==2); % all novel & trained
 y2 = data(:,idx,1);
+err_y2 = data(:,idx,3);
 [~,tend2] = max(y2);
-[params2, ~, ~] = fitExpSaturation(t(tstart:tend2), y2(tstart:tend2), 0);
+% tend2 = tend2+1;
+[params2, yfit2, ~] = fitExpSaturation(t(tstart:tend2), y2(tstart:tend2), 0);
 
 idx = find(idx_by_stimgroup==7 & idx_by_subjectgroup==1); % all novel & naive
 y3 = data(:,idx,1);
+err_y3 = data(:,idx,3);
 [~,tend3] = max(y3);
-[params3, ~, ~] = fitExpSaturation(t(tstart:tend3), y3(tstart:tend3), 0);
+% tend3 = tend3+1;
+[params3, yfit3, ~] = fitExpSaturation(t(tstart:tend3), y3(tstart:tend3), 0);
 
 hf = figure;
 set(gcf, 'color', cfg.bgcol); 
 
-subplot(121); hold on
-b(1) = plot(t(tstart:tend1), y1(tstart:tend1));
-b(2) = plot(t(tstart:tend2), y2(tstart:tend2));
-b(3) = plot(t(tstart:tend3), y3(tstart:tend3));
+subplot(121); hold on; clear b
+b(1) = scatter(t(tstart:tend1), y1(tstart:tend1),'b','filled');
+errorbar(t(tstart:tend1), y1(tstart:tend1),err_y1(tstart:tend1), ...
+    'vertical', 'LineStyle', 'none','Color','b');
+b(2) = scatter(t(tstart:tend2), y2(tstart:tend2),'r','filled');
+errorbar(t(tstart:tend2), y2(tstart:tend2),err_y2(tstart:tend2), ...
+    'vertical', 'LineStyle', 'none','Color','r');
+b(3) = scatter(t(tstart:tend3), y3(tstart:tend3),'g','filled');
+errorbar(t(tstart:tend3), y3(tstart:tend3),err_y3(tstart:tend3), ...
+    'vertical', 'LineStyle', 'none','Color','g');
+plot(t(tstart:tend1), yfit1,'b--')
+plot(t(tstart:tend2), yfit2,'r--')
+plot(t(tstart:tend3), yfit3,'g--')
 axis tight
 legend(b,{'familiar/trained','novel/trained','novel/naive'})
-xlabel('Time from stim. onset');
-ylabel('Avg. intertrial similarity (same odor)');
+xlabel('Time from stim. onset [s]');
+ylabel('Avg. intertrial similarity (same odor) + STD');
 set(gca, 'color', cfg.bgcol, 'XColor',cfg.axcol, 'YColor',cfg.axcol, 'ZColor',cfg.axcol);
 
 subplot(122)
 bar([params1;params2;params3]')
-xticklabels({'amplitude','tau','offset'})
+xticklabels({'amplitude','1/tau','offset'})
 box off
 ylabel('Fitted value');
 set(gca, 'color', cfg.bgcol, 'XColor',cfg.axcol, 'YColor',cfg.axcol, 'ZColor',cfg.axcol);
